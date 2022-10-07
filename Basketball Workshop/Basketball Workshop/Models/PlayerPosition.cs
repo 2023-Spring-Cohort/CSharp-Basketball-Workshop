09b03e22c8cc05f73994ef02a03c5b9ab605583b
Jim Kocsis <jim@wecancodeit.org>
2022-10-07 12:06:37 -0400

    Initial Commit

---
+++ Basketball Workshop/Basketball Workshop/Models/PlayerPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace Basketball_Workshop.Models
+{
+    public class PlayerPosition
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; }
+        public int PositionId { get; set; }
+        public virtual Position Position { get; set; }
+    }
+}
+
+/* 
+ Id     PlayerId    Player (Info from Player Class)     PositionId      Position (Info from Position Class)
+ 1          1       Jim Kocsis                              1           Point Guard
+ 
+ */
